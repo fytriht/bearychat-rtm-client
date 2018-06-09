76f30ec032940721d1a131007f94bc452b959b26
Haiyang Peng <phycgp@live.com>
2018-06-09 20:06:52 +0800

    refactor: rename variable

--- src/withTimeout.js
+++ src/withTimeout.js
@@ -1,7 +1,7 @@
 const delay = require('./delay');
 
-async function withTimeout(ms, rejectMessage, promise) {
-  const timeoutPromise = delay.reject(ms, rejectMessage);
+async function withTimeout(ms, rejectReason, promise) {
+  const timeoutPromise = delay.reject(ms, rejectReason);
   const result = await Promise.race([promise, timeoutPromise]);
   timeoutPromise.cancel();
   return result;
