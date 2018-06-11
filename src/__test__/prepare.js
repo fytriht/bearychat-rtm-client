fa3c705f6d13a8e0414c24af431bfc42cb41e56d
Haiyang Peng <phycgp@live.com>
2018-06-11 18:56:58 +0800

    test: add test ping timeout

--- src/__test__/prepare.js
+++ src/__test__/prepare.js
@@ -2,8 +2,9 @@ process.on('unhandledRejection', (reason) => {
   throw reason;
 });
 
-import createMockServer, { CLIENT_PING_INTERVAL, BACKOFF_MULTIPLIER } from './createMockServer';
+import createMockServer, { CLIENT_PING_INTERVAL, CLIENT_PING_TIMEOUT, BACKOFF_MULTIPLIER } from './createMockServer';
 
 global.createMockServer = createMockServer;
 global.CLIENT_PING_INTERVAL = CLIENT_PING_INTERVAL;
+global.CLIENT_PING_TIMEOUT = CLIENT_PING_TIMEOUT;
 global.BACKOFF_MULTIPLIER = BACKOFF_MULTIPLIER;
